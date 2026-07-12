0b85d3d98ec416dfa32d9ee329973d34740218be
rahel <rahelkempf@gmx.ch>
2026-07-12 15:30:25 +0000

    temp upload results

---
+++ openCLIP/domain_transfer/10c_cosplay_then_character_fine_tuning.bash
@@ -0,0 +1,29 @@
+source /home/ubuntu/fcore/.venv/bin/activate
+
+# enter the src folder of the open_clip repository
+cd /home/ubuntu/open_clip/src
+
+# specify which GPUs you want to use.
+export CUDA_VISIBLE_DEVICES=0,1
+export WANDB_PROJECT=fcore_openclip_finetuning
+
+torchrun --nproc_per_node 2 -m open_clip_train.main -- \
+  --name 10c_cosplay_then_character_train \
+  --seed 42 \
+  --dataset-type csv \
+  --train-data /home/ubuntu/data/domain_transfer_10c_split/character_metadata.csv \
+  --csv-img-key filepath \
+  --csv-caption-key caption \
+  --csv-separator "," \
+  --model hf-hub:timm/ViT-B-16-SigLIP2-256 \
+  --pretrained /home/ubuntu/open_clip/logs/10c_cosplay_finetuning/V1_10c_cosplay_train/checkpoints/epoch_5.pt \
+  --batch-size 32 \
+  --precision amp \
+  --workers 4 \
+  --warmup 50 \
+  --lr 1e-6 \
+  --wd 0.1 \
+  --epochs 20 \
+  --save-frequency 1 \
+  --report-to wandb \
+  --logs /home/ubuntu/open_clip/logs/10c_cosplay_then_character_finetuning
